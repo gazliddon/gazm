a401be53263c9b6cfb34f3817df49a16a966fcde
Gaz Liddon Blackbook Pro <gaz@gazaxian.com>
2023-11-21 08:41:48 +0000

    Adding test code back in

--- gazm/src/frontend/testit.rs
+++ gazm/src/frontend/testit.rs
@@ -1,34 +1,34 @@
 // #![deny(unused_imports)]
 
+
+use std::fs;
 use crate::opts::Opts;
+use crate::ast::{Ast, iter_ids_recursive };
 
+use super::{ create_source_file, TokenizeRequest, TokenizeResult, FeResult, Item };
 
-pub fn test(_opts: &Opts) {
-    // let text = fs::read_to_string(&opts.project_file).unwrap();
-    // let mut sf = create_source_file(&text);
-    // sf.file = opts.project_file.clone();
+pub fn test(opts: &Opts) {
+    let text = fs::read_to_string(&opts.project_file).unwrap();
+    let mut sf = create_source_file(&text);
+    sf.file = opts.project_file.clone();
 
-    // let req = TokenizeRequest::for_single_source_file(sf, opts);
-    // let res: GResult<TokenizeResult> = req.try_into();
+    let req = TokenizeRequest::for_single_source_file(sf, opts);
+    let res: FeResult<TokenizeResult> = req.try_into();
 
-    // match res {
-    //     Ok(tres) => {
-    //         let mut tree = Ast::from_node(&tres.node);
-    //         // Strip any ycomments
-    //         let it = iter_ids_recursive(tree.tree.root());
-    //         tree.detach_nodes_filter(it, |n| matches!(n.value().item, Item::Comment(..)));
-    //         println!("Parsed fine!");
-    //         println!("{}", tree)
-    //     }
+    match res {
+        Ok(tres) => {
+            let mut tree = Ast::from_node(&tres.node);
+            // Strip any ycomments
+            let it = iter_ids_recursive(tree.tree.root());
+            tree.detach_nodes_filter(it, |n| matches!(n.value().item, Item::Comment(..)));
+            println!("Parsed fine!");
+            println!("{}", tree)
+        }
 
-    //     Err(GazmErrorKind::ParseError(e)) => println!(
-    //         "Failed : line: {} col: {}",
-    //         e.pos.line() + 1,
-    //         e.pos.col() + 1
-    //     ),
-
-    //     Err(e) => {
-    //         println!("Dunno:\n{e}")
-    //     }
-    // }
+        Err(e) => println!(
+            "{e}\nFailed : line: {} col: {}",
+            e.position.line() + 1,
+            e.position.col() + 1
+        ),
+    }
 }
