5f7a04c9ecc098da44b784751ffdc3c0fc6d64bc
Gaz Liddon Blackbook Pro <gaz@gazaxian.com>
2024-01-21 07:56:17 +0000

    Misc
    Adding trait for front end ops
    Isolatiing 6809 specific errors

---
+++ gazm/src/cpu6809/frontend/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+use super::AddrModeParseType;
+
+#[derive(Debug, Error, Clone, PartialEq, Copy)]
+pub enum Cpu6809AssemblyErrorKind {
+    #[error("This {0:?} is not supported for this opcode")]
+    ThisAddrModeUnsupported(AddrModeParseType),
+    #[error("Addressing mode is not supported for this opcode")]
+    AddrModeUnsupported,
+    #[error("This instruction only supports inherent mode addressing")]
+    OnlySupports(AddrModeParseType),
+}
---
+++ gazm/src/frontend/traits.rs
@@ -0,0 +1,8 @@
+use crate::frontend::{ TSpan, Node, PResult };
+
+pub trait FrontendCpuTrait<K> {
+    fn parse_command(_input: TSpan) -> PResult<Node>  {
+        panic!()
+    }
+}
+
